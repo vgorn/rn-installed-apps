10c7abbc6779bbd1eb892b5c7e58fb389be5ffb5
vgorn <vladimirgorn@gmail.com>
2023-12-07 12:16:06 +0700

    Update RNInstalledAppsPackage.java

--- android/src/main/java/com/jstokes/RNInstalledAppsPackage.java
+++ android/src/main/java/com/jstokes/RNInstalledAppsPackage.java
@@ -16,7 +16,6 @@ public class RNInstalledAppsPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNInstalledAppsModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
